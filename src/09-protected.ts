4e9d5336a4c1170da857cf807a4999691d907ec4
Gisele Galaburri <gisele.galaburri89@gmail.com>
2022-06-20 19:50:15 -0400

    protected accesor basics

---
+++ src/09-protected.ts
@@ -0,0 +1,45 @@
+export class Animal {
+  constructor(protected name: string) {}
+
+  move(){
+    console.log('Moving along!');
+  }
+
+  greeting() {
+    return `Hello, I'm ${this.name}`;
+  }
+
+  protected doSomething() {
+    console.log('doing...!');
+  }
+}
+
+// similar to Java
+export class Dog extends Animal {
+
+  constructor(
+    name: string,
+    public owner: string,
+  ) {
+    super(name);
+  }
+  woof(times: number): void {
+    for (let index = 0; index < times; index++) {
+      console.log(`Woof! ${this.name}`);
+    }
+
+    this.doSomething();
+  }
+
+  move() {
+    console.log('Moving as a dog..')
+    super.move();
+  }
+}
+
+
+const myDog = new Dog('Chiquito', 'Agustin');
+myDog.woof(1);
+// myDog.name = 'Lolapolola'
+// myDog.doSomething(); // this doesn't work cause protected rules
+myDog.move();
