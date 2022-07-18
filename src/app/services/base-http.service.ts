523e1923a5b35bc7e77a7644c65fe7dfb33ee438
Gisele Galaburri <gisele.galaburri89@gmail.com>
2022-07-18 19:01:38 -0400

    basics: use generic methods

--- src/app/services/base-http.service.ts
+++ src/app/services/base-http.service.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { UpdateProductDto } from "../dtos/product.dto";
 
 import { Category } from './../models/category.model'
 import { Product } from './../models/product.model'
@@ -12,6 +13,12 @@ export class BaseHttpService<T> {
 
   async getAll() {
     const { data } = await axios.get<T[]>(this.url);
+    return data;
+  }
+
+  // this generic name must be different from the class name
+  async update<ID, DTO>(id: ID, changes: DTO) {
+    const { data } = await axios.put(`${this.url}/${id}`, changes);
     return data;
   }
 }
@@ -31,6 +38,12 @@ export class BaseHttpService<T> {
 
   const rta = await productService.getAll();
   console.log('products', rta.length);
+
+  // here we are saying what is the that must infer/use
+  productService.update<Product['id'], UpdateProductDto>(1, {
+    title: 'nuevo titulo',
+    description: 'nueva descripcion',
+  });
 
   const url2 = 'https://api.escuelajs.co/api/v1/categories'
   const categoryService = new BaseHttpService<Category>(url2);
